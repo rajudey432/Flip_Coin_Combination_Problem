b90487bd7daa37b65415757820446447811e589e
rajudey432 <rajudey432@.com>
2023-03-22 20:30:16 +0530

    singlet shows

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
 
-declare -A boublet_combinations
+# simulate flipping a coin
+flip=$(( RANDOM % 2 ))
 
-echo "Enter the number of times to flip the coin:"
-read num_flips
-
-for (( i=0; i<$num_flips; i++ ))
-do
-	flip1=$(( RANDOM % 2 ))
-	flip2=$(( RANDOM % 2 ))
-	doublet="$flip1$flip2"
-
-	if [[ ${doublet_combinations[$doublet]+_} ]];
+# display the result
+if [ $flip -eq 0 ];
 then
-	doublet_combinations[$doublet]=$((doublet_combinations[$doublet]+1))
+	echo "Heads"
 else
-	doublet_combinations[$doublet]=1
+	echo "Tails"
 fi
-done
-
-	echo "Doublet_combinations:"
-for key in "${!doublet_combinations[@]}"
-do
-	percentage=$(echo "scale=2; ${doublet_combinations[$key]} / $sum_flips * 100 )
-echo	"$key: ${doublet_combinations[$key]} (${percentage}%)"
-done
